edb45105112b08031baf2eca1403cecb523a3c45
Gregory Gerard <wako057@gmail.com>
2024-02-21 23:22:27 +0100

    feat: add sign/verify

--- src/crypt/controllers/encrypt/encrypt.controller.ts
+++ src/crypt/controllers/encrypt/encrypt.controller.ts
@@ -1,12 +1,15 @@
 import {
+  BadRequestException,
   Body,
   Controller,
+  HttpCode,
   Post,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { CryptService } from '../../services/crypt.service';
 import { FlexibleInputDto } from '../../dto/flexibleinput.dto';
+import { SignatureDto } from '../../dto/signature.dto';
 
 @Controller()
 export class EncryptController {
@@ -23,5 +26,21 @@ export class EncryptController {
   @UsePipes(new ValidationPipe())
   decrypt(@Body() dataPosted: FlexibleInputDto) {
     return this.cryptService.decrypt(dataPosted);
+  }
+
+  @Post('sign')
+  @UsePipes(new ValidationPipe())
+  sign(@Body() dataPosted: FlexibleInputDto) {
+    console.log(dataPosted);
+    return this.cryptService.sign(dataPosted);
+  }
+
+  @Post('verify')
+  @UsePipes(new ValidationPipe())
+  @HttpCode(204)
+  verify(@Body() dataPosted: SignatureDto) {
+    if (false === this.cryptService.verify(dataPosted)) {
+      throw new BadRequestException('Signature Mismatch');
+    }
   }
 }
