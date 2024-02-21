987ff8a901d8d56f1bd2bf91b7c3218196fd18dc
Gregory Gerard <wako057@gmail.com>
2024-02-21 21:07:45 +0100

    fixup! test(base64): test base64 encrypt decrypt

--- src/crypt/providers/base64/base64.ts
+++ src/crypt/providers/base64/base64.ts
@@ -6,14 +6,12 @@ export class Base64 implements IcryptInterface {
   encrypt(input: string): string {
     const back: string = Buffer.from(input).toString('base64');
 
-    console.log(`encrypt: input: ${input} - back: ${back}`);
     return back;
   }
 
   decrypt(input: string): string {
     const back: string = Buffer.from(input, 'base64').toString();
 
-    console.log(`decrypt: input: ${input} - back: ${back}`);
     return back;
   }
 }
