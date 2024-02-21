a14880ca3fc001494e0773f31123b361eb46e765
Gregory Gerard <wako057@gmail.com>
2024-02-21 17:36:07 +0100

    feat: add encrypt/decrypt endpoint

--- src/crypt/controllers/encrypt/encrypt.controller.ts
+++ src/crypt/controllers/encrypt/encrypt.controller.ts
@@ -1,14 +1,27 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { CryptService } from '../../services/crypt.service';
+import { FlexibleInputDto } from '../../dto/flexibleinput.dto';
 
-@Controller('encrypt')
+@Controller()
 export class EncryptController {
   constructor(private cryptService: CryptService) {}
 
-  @Post()
-  encrypt(@Body() datasPosted: any) {
-    console.log(datasPosted);
-    this.cryptService.ecnryptThis(datasPosted);
-    return { myData: 'blublu' };
+  @Post('encrypt')
+  @UsePipes(new ValidationPipe())
+  encrypt(@Body() dataPosted: FlexibleInputDto) {
+    console.log(`dataPosted: ${JSON.stringify(dataPosted)}`);
+    return this.cryptService.encrypt(dataPosted);
+  }
+
+  @Post('decrypt')
+  @UsePipes(new ValidationPipe())
+  decrypt(@Body() dataPosted: FlexibleInputDto) {
+    return this.cryptService.decrypt(dataPosted);
   }
 }
