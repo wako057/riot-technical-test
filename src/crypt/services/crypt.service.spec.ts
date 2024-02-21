93fa5f7028c8958fe2021501ae3ca7b13e21646e
Gregory Gerard <wako057@gmail.com>
2024-02-21 21:08:22 +0100

    test(cryptService): add encrypt/decrypt

--- src/crypt/services/crypt.service.spec.ts
+++ src/crypt/services/crypt.service.spec.ts
@@ -1,9 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CryptService } from './crypt.service';
 import { Base64 } from '../providers/base64/base64';
+import { FlexibleInputDto } from '../dto/flexibleinput.dto';
 
 describe('ServicesService', () => {
   let service: CryptService;
+  const dataDecoded: FlexibleInputDto = {
+    key1: 'bla',
+    key2: 'blu',
+    key3: {
+      key4: 'bli',
+      key5: 'blo',
+    },
+  };
+  const dataEncoded: FlexibleInputDto = {
+    key1: 'Ymxh',
+    key2: 'Ymx1',
+    key3: 'eyJrZXk0IjoiYmxpIiwia2V5NSI6ImJsbyJ9',
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -15,5 +29,18 @@ describe('ServicesService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('encrypt', () => {
+    const result: FlexibleInputDto = service.encrypt(dataDecoded);
+    expect(result.key1).toEqual('Ymxh');
+    expect(result.key2).toEqual('Ymx1');
+    expect(result.key3).toEqual('eyJrZXk0IjoiYmxpIiwia2V5NSI6ImJsbyJ9');
+  });
+
+  it('decrypt', () => {
+    const result: FlexibleInputDto = service.decrypt(dataEncoded);
+
+    expect(result).toEqual(dataDecoded);
   });
 });
